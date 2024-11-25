ea657932c893bf9ab280c48f8d82bb541c3f8a99
Monika-SV <monikavenugopal27@gmail.com>
2024-11-25 14:55:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function increaseFontSize() {
+    document.body.style.fontSize = "larger";
+}
+
+function decreaseFontSize() {
+    document.body.style.fontSize = "smaller";
+}
